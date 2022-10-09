0246fcfefcb6e9870dc3b39e149214aa894b3829
JMBalanzar <jmbalanzar@gmail.com>
2022-10-08 20:37:50 -0500

    Dashboard initialization before typescript

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,49 +1,61 @@
-import jsPDF from 'jspdf';
-import html2canvas from 'html2canvas';
-import { ComposableMap, Geographies, Geography } from "react-simple-maps"
+// import jsPDF from 'jspdf';
+// import html2canvas from 'html2canvas';
+// import { ComposableMap, Geographies, Geography } from "react-simple-maps"
 
-const geoUrl = "https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json"
+// const geoUrl = "https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json"
 
-const MapChart = () => {
-  return (
-    <ComposableMap>
-      <Geographies geography={geoUrl}>
-        {({ geographies }) =>
-          geographies.map((geo) => (
-            <Geography key={geo.rsmKey} geography={geo} />
-          ))
-        }
-      </Geographies>
-    </ComposableMap>
-  )
-}
+// const MapChart = () => {
+//   return (
+//     <ComposableMap>
+//       <Geographies geography={geoUrl}>
+//         {({ geographies }) =>
+//           geographies.map((geo) => (
+//             <Geography key={geo.rsmKey} geography={geo} />
+//           ))
+//         }
+//       </Geographies>
+//     </ComposableMap>
+//   )
+// }
 
-const App = () => {
-  const printDocument = () => {
-    const input = document.getElementById('divToPrint');
-    html2canvas(input)
-      .then((canvas) => {
-        const imgData = canvas.toDataURL('image/png');
-        const pdf = new jsPDF();
-        pdf.addImage(imgData, 'JPEG', 0, 0);
-        // pdf.output('dataurlnewwindow');
-        pdf.save("dashboard.pdf");
-      })
-    ;
-  }//<iframe width="560" height="315" src="https://www.youtube.com/embed/wKRL7vkWMoc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+// const Appp = () => {
+//   const printDocument = () => {
+//     const input = document.getElementById('divToPrint');
+//     html2canvas(input)
+//       .then((canvas) => {
+//         const imgData = canvas.toDataURL('image/png');
+//         const pdf = new jsPDF();
+//         pdf.addImage(imgData, 'JPEG', 0, 0);
+//         // pdf.output('dataurlnewwindow');
+//         pdf.save("dashboard.pdf");
+//       })
+//     ;
+//   }//<iframe width="560" height="315" src="https://www.youtube.com/embed/wKRL7vkWMoc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+//   return (
+//     <div>
+//       <div id='divToPrint'>
+//         <h1>Dashboard</h1>
+//         <p>Some content</p>
+//         <div style={{width:'500px'}}>
+//           <MapChart/>
+//         </div>
+        
+//       </div>
+//       <button  onClick={printDocument}>Download PDF</button>
+//     </div>
+//   );
+// }
+
+
+
+import Layout from "./components/Layout";
+
+const App = () =>{
   return (
     <div>
-      <div id='divToPrint'>
-        <h1>Dashboard</h1>
-        <p>Some content</p>
-        <div style={{width:'500px'}}>
-          <MapChart/>
-        </div>
-        
-      </div>
-      <button  onClick={printDocument}>Download PDF</button>
+      <Layout/>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ dashboard/src/components/Layout.js
@@ -0,0 +1,15 @@
+const Layout = () => {
+    return (
+      <div 
+        style={{
+          margin:'10px',
+          height:'98vh',
+          border:'3px dotted #dfdfdf',
+        }}
+      >
+        Hello
+      </div>
+    )
+}
+
+export default Layout;
